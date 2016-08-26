83584cf49c4ea2198c30bc70660ef54d3e433e1c
hapfelmeier <hapfelmeier@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-08-26 10:56:43 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rseg@9 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/plot.R
+++ pkg/R/plot.R
@@ -12,7 +12,6 @@
 #' @param ... additional arguments passed to \link[partykit]{plot.constparty}.
 #'
 #' @export
-#' @import stats
 #' @import graphics
 #'
 #' @examples
@@ -21,9 +20,7 @@
 #' airct
 #' plot(airct)
 plot.segmentation <- function(x, ...) {
-  nouts <- length(unlist(strsplit(as.character(formula(x[[1]][[1]])[2]), "[+]")))
-  print(nouts)
-  if (nouts > 1) stop("plot function does currently not support multivariate outcomes")
+  if (attr(formula(x[[1]][[1]]), "response") == 0) stop("plot function does currently not support multivariate outcomes")
   if (nrow(x[[1]][[1]]$data) == 0) {
     dat <- x[[1]][[1]]$data.save
     dat$segment <- as.factor(predict.segmentation(x, type = "segment"))
@@ -31,7 +28,7 @@ plot.segmentation <- function(x, ...) {
     } else {
       dat <- x[[1]][[1]]$data
       dat$segment <- as.factor(predict.segmentation(x, type = "segment"))
-      tree <- ctree(dat[, as.character(formula(x[[1]][[1]])[2])] ~ segment, data = dat, minsplit = 2, minbucket = 1, stump = TRUE, mincriterion = 0, multiway = TRUE)
+      tree <- ctree(dat[, capture.output(formula(x[[1]][[1]])[[2]])] ~ segment, data = dat, minsplit = 2, minbucket = 1, stump = TRUE, mincriterion = 0, multiway = TRUE)
     }
     plot(tree, ip_args = list(pval = FALSE, id = FALSE), tp_args = list(id = FALSE), ...)
 }
